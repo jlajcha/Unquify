4f37574d2cab60b75be8e47208d309dce634caf9
matotec <matias.chaile.1988@gmail.com>
2020-11-03 15:10:44 -0300

    muchos endpoints sin exception

--- Api/apiUnqfy.js
+++ Api/apiUnqfy.js
@@ -2,36 +2,235 @@ const {getUNQfy, saveUNQfy} = require('../persistenceUNQfy');
 const express = require('express');
 
 const artists = express.Router();
+const albums = express.Router();
+const users = express.Router();
 
-const unqfy1 = getUNQfy();
+let unqfy = getUNQfy();
 
-artists.get('/artists',
-(req, res) =>{
+
+//endpoints artists
+artists.route('/artists')
+.post((req, res) => {
+        const data = req.body;
+        let artistRes;
+        if(data.name === undefined || data.country === undefined){
+            //hacer exception handler
+            return;
+        }
+        try{
+            artistRes = unqfy.addArtist(data);
+        }catch(err){
+            //hacer exception handler
+            return;
+        }
+    saveUNQfy(unqfy);
+    unqfy = getUNQfy();
+    res.status(201);
+    res.json(artistRes.toJSON());
+})
+.get((req, res) =>{
     let name = req.query.name;
     if(name === undefined){
         name = "";
     }
-    const unqfy = getUNQfy();
-    const artistsSearch = unqfy1.searchArtistsByName(name);
-    
+    const artistsSearch = unqfy.searchArtistsByName(name);
+
     res.status(200);
-    res.json(artistsSearch.map(artist => artist.toJSON()));
-        // artistsSearch.map(artist => artist.toJSON())
-    
+    res.json(artistsSearch.map(artist => artist.toJSON()));    
 })
 
 artists.route('/artists/:artistId')
 .get((req, res) => {
     const artistId = parseInt(req.params.artistId);
-    const unqfy = getUNQfy();
-    const searchArtist = unqfy.getArtistById(artistId);
+    let searchArtist;
+    try{
+        searchArtist = unqfy.getArtistById(artistId);
+    }catch(err){
+        //hacer exception handler
+        return;
+    }    
 
     res.status(200);
     res.json(
         searchArtist.toJSON()
     );
 })
+.patch((req, res) => {
+    const data = req.body;
+    const artistId = parseInt(req.params.artistId);
+    let artistRes;
+        if(data.name === undefined || data.country === undefined){
+            //hacer exception handler
+            return;
+        }
+        try{
+            unqfy.updateArtistName(artistId, data.name);
+            unqfy.updateArtistNationality(artistId, data.country);
+            artistRes = unqfy.getArtistById(artistId);
+        }catch(err){
+            //hacer exception handler
+            return;
+        }
+    saveUNQfy(unqfy);
+    unqfy = getUNQfy();
+    res.status(200);
+    res.json(artistRes.toJSON());
+})
+.delete((req, res) => {
+    const artistId = parseInt(req.params.artistId);
+    unqfy.deleteArtistWithId(artistId);
+
+    saveUNQfy(unqfy);
+    unqfy = getUNQfy();
+    res.status(204);
+    res.send({success: 'true'});
+})
+
+//endpoints albums
+albums.route('/albums')
+.post((req, res) => {
+        const data = req.body;
+        let albumRes;
+        if(data.artistId === undefined || data.name === undefined || data.year === undefined){
+            //hacer exception handler
+            return;
+        }
+        try{
+            albumRes = unqfy.addAlbum(data.artistId, data);
+        }catch(err){
+            //hacer exception handler
+            return;
+        }
+    saveUNQfy(unqfy);
+    unqfy = getUNQfy();
+    res.status(201);
+    res.json(albumRes.toJSON());
+})
+.get((req, res) =>{
+    let name = req.query.name;
+    if(name === undefined){
+        name = "";
+    }
+    const artistsSearch = unqfy.searchAlbumsByName(name);
+
+    res.status(200);
+    res.json(artistsSearch.map(artist => artist.toJSON()));    
+})
+
+albums.route('/albums/:albumId')
+.get((req, res) => {
+    const albumId = parseInt(req.params.albumId);
+    let searchAlbum;
+    try{
+        searchAlbum = unqfy.getAlbumById(albumId);
+    }catch(err){
+        //hacer exception handler
+        return;
+    }    
+
+    res.status(200);
+    res.json(
+        searchAlbum.toJSON()
+    );
+})
+.patch((req, res) => {
+    const data = req.body;
+    const albumId = parseInt(req.params.albumId);
+    let albumRes;
+        if(data.year === undefined){
+            //hacer exception handler
+            return;
+        }
+        try{
+            unqfy.updateAlbumYear(albumId, data.year);
+            albumRes = unqfy.getAlbumById(albumId);
+        }catch(err){
+            //hacer exception handler
+            return;
+        }
+    saveUNQfy(unqfy);
+    unqfy = getUNQfy();
+    res.status(200);
+    res.json(albumRes.toJSON());
+})
+.delete((req, res) => {
+    const albumId = parseInt(req.params.albumId);
+    unqfy.deleteAlbumWithId(albumId);
+
+    saveUNQfy(unqfy);
+    unqfy = getUNQfy();
+    res.status(204);
+    res.send({success: 'true'});
+})
+
+//endpoints users
+users.route('/users/:userId')
+.get((req, res) => {
+    const userId = parseInt(req.params.userId);
+    let userRes;
+    try{
+        userRes = unqfy.getUserById(userId);
+    }catch(err){
+        //hacer exception handler
+        return;
+    }
+
+    res.status(200);
+    res.json(userRes.toJSON());
+})
+.patch((req, res) => {
+    const data = req.body;
+    const userId = parseInt(req.params.userId);
+    let userRes;
+        if(data.name === undefined){
+            //hacer exception handler
+            return;
+        }
+        try{
+            unqfy.updateUserName(userId, data.name);
+            userRes = unqfy.getUserById(userId);
+        }catch(err){
+            //hacer exception handler
+            return;
+        }
+    saveUNQfy(unqfy);
+    unqfy = getUNQfy();
+    res.status(200);
+    res.json(userRes.toJSON());
+})
+.delete((req, res) =>{
+    const userId = parseInt(req.params.userId);
+    unqfy.deleteUserWithId(userId);
+
+    saveUNQfy(unqfy);
+    unqfy = getUNQfy();
+    res.status(204);
+    res.send({success: 'true'});
+})
+
+users.route('/users')
+.post((req, res) => {
+    const data = req.body;
+    let userRes;
+        if(data.name === undefined){
+            //hacer exception handler
+            return;
+        }
+        try{
+            userRes = unqfy.addUser(data.name);
+        }catch(err){
+            //hacer exception handler
+            return;
+        }
+    saveUNQfy(unqfy);
+    unqfy = getUNQfy();
+    res.status(201);
+    res.json(userRes.toJSON());
+})
+
 
 module.exports = {
-    artists
+    artists,
+    albums,
+    users
 };
--- apiController.js
+++ apiController.js
@@ -3,17 +3,19 @@ const express = require('express');
 const app = express();
 
 const {
-    artists
+    artists,
+    albums,
+    users
 } = require('./Api/apiUnqfy');
 
 const port = process.env.Port || 8080;
 
 app.use(bodyParser.json());
 
-app.use('/api', artists);
+app.use('/api', artists, albums, users);
 
 const server = app.listen(port, () => {
     console.log("Server listen");
 });
 
-console.log("Servicio corriendo en el puerto", port)
+console.log("Servicio corriendo en el puerto", port);
